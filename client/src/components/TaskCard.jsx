9fc35699ad38121eeb873210ad0f907430a9eb03
Deivyansh Singh <106349960+striver-24@users.noreply.github.com>
2024-07-18 15:27:21 +0530

    TaskCard Commit

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {
+    MdAttachFile,
     MdKeyboardArrowDown,
     MdKeyboardArrowUp,
     MdKeyboardDoubleArrowUp,
@@ -28,6 +29,7 @@ const TaskCard = ({ task }) => {
 
                     {user?.isAdmin && <TaskDialog task={task} />}
                 </div>
+
                 <>
                     <div className='flex items-center gap-2'>
                         <div className={clsx("w-4 h-4 rounded-full", TASK_TYPE[task.stage])} />
@@ -39,7 +41,61 @@ const TaskCard = ({ task }) => {
                         {formatDate(new Date(task?.date))}
                     </span>
                 </>
+
+                <div className='w-full border-t border-gray-200 my-2' />
+                    <div className='flex items-center justify-between mb-2'>
+                        <div className='flex items-center gap-3'>
+                            <div className='flex gap-1 items-center text-sm text-gray-600'>
+                                <BiMessageAltDetail />
+                                <span>{task?.activities?.length}</span>
+                            </div>
+                            <div className='flex gap-1 items-center text-sm text-gray-600'>
+                                <MdAttachFile />
+                                <span>{task?.attachments?.length}</span>
+                            </div>
+                            <div className='flex gap-1 items-center text-sm text-gray-600'>
+                                <FaList />
+                                <span>0/{task?.subTasks?.length}</span>
+                            </div>
+                        </div>
+
+                        <div className='flex flex-row-reverse'>
+                            {task?.team?.map((m, index) => (
+                                <div key={index} className={clsx("w-7 h-7 rounded-full text-white flex items-center justify-center text-sm -mr-1", BGS[index % BGS?.length])}>
+                                    <UserInfo user={m} /> 
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+
+                    {/* sub tasks */}
+                    {task?.subTasks?.length > 0 ? ( 
+                        <div className='py-4 border-t border-gray-200'>
+                            <h5 className='text-base line-clamp-1 text-black'>{task?.subTasks[0].title}</h5>
+                            <div className='p-4 space-x-8'>
+                                <span className='text-sm text-gray-600'>{formatDate(new Date(task?.subTasks[0]?.date))}</span>
+                                <span className='bg-blue-600/10 px-3 py-1 rounded-full text-red-500 font-medium'>{task?.subTasks[0].tag}</span>
+                            </div>
+                        </div>
+                    ) : (
+                        <>
+                            <div className='py-4 border-t border-gray-200'>
+                                <span className='text-gray-500'>
+                                    No Sub Task
+                                </span>
+                            </div>
+                        </>
+                    )}
+
+                    <div className='w-full pb-2'>
+                        <button disabled={user.isAdmin ? false : true} className='w-full flex gap-4 items-center text-sm text-gray-500 font-semibold disabled:cursor-not-allowed disabled:text-gray-300'>
+                            <IoMdAdd className='text-lg'/>
+                            <span>ADD SUBTASK</span>
+                        </button>
+                    </div>
             </div>
+
+            {/* <AddSubTask open={open} setOpen={setOpen} id={task._id} /> */}
         </> 
     );
 };

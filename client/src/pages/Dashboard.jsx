8927957a523df53e25b05b8cbda45d72a205af87
Deivyansh Singh <106349960+striver-24@users.noreply.github.com>
2024-07-18 10:23:53 +0530

    Dashboard Final Commits

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -16,7 +16,7 @@ import { BGS, PRIORITYSTYLES, TASK_TYPE, getInitials } from '../utils';
 import UserInfo from '../components/UserInfo';
 import { getInitials } from '../utils';
 
-const TaskTable = ({tasks}) => {
+const TaskTable = ({ tasks }) => {
   const ICONS = {
     high: <MdKeyboardDoubleArrowUp />,
     medium: <MdKeyboardArrowDown />,
@@ -34,7 +34,7 @@ const TaskTable = ({tasks}) => {
     </thead>
   );
 
-  const TableRow = ({task}) => (
+  const TableRow = ({ task }) => (
     <tr className='border-b border-gray-300 text-gray-600 hover:bg-gray-300/10'>
       <td className='py-2'>
         <div className='flex items-center gap-2'>
@@ -86,10 +86,10 @@ const TaskTable = ({tasks}) => {
   )
 } 
 
-const UserTable = ({users}) => {
+const UserTable = ({ users }) => {
   const TableHeader = () => (
-    <thead className='border-b border-gray-300 dark:border-gray-600'>
-      <tr className='text-black dark:text-white text-left'>
+    <thead className='border-b border-gray-300'>
+      <tr className='text-black text-left'>
         <th className='py-2'>Full Name</th>
         <th className='py-2'>Status</th>
         <th className='py-2'>Created At</th>
@@ -97,15 +97,25 @@ const UserTable = ({users}) => {
     </thead>
   );
 
-  const TableRow = ({user}) => (
+  const TableRow = ({ user }) => (
     <tr className='border-b border-gray-200 text-gray-600 hover:bg-gray-400/10'>
       <td className='py-2'>
         <div className='flex items-center gap-3'>
           <div className='w-9 h-9 rounded-full text-white flex items-center justify-center text-sm bg-violet-700'>
             <span className='text-center'>{getInitials(user?.name)}</span>
           </div>
+          <div>
+            <p>{user.name}</p>
+            <span className='text-xs text-black'>{user?.role}</span>
+          </div>
         </div>
       </td>
+      <td>
+        <p className={clsx("w-fit px-3 py-1 rounded-full text-sm", user?.isActive ? "bg-blue-200" : "bg-yellow-100")}>
+          {user?.isActive ? "Active" : "Disabled"}
+        </p>
+      </td>
+      <td className='py-2 text-sm'>{moment(user?.createdAt).fromNow()}</td>
     </tr>
   )
 
@@ -156,7 +166,7 @@ const Dashboard = () => {
     },
   ];
   
-  const Card = ({label, count, bg, icon}) => {
+  const Card = ({ label, count, bg, icon }) => {
     return (
       <div className='w-full h-32 bg-white p-5 shadow-md rounded-md flex items-center justify-between'>
         <div className='h-full flex flex-1 flex-col justify-between'>

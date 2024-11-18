c66cc3f303d8723216da6ab0c49b8a526680e774
Deivyansh Singh <106349960+striver-24@users.noreply.github.com>
2024-11-18 18:13:39 +0530

    App Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-import {Routes, Route, Navigate, Outlet, useLocation} from "react-router-dom";
+import { Routes, Route, Navigate, Outlet, useLocation } from "react-router-dom";
 import { Toaster } from "sonner";
 import { useSelector, useDispatch } from "react-redux";
 import { setOpenSidebar } from './redux/slices/authSlice';
 import Login from './pages/Login';
 import Dashboard from "./pages/dashboard";
-import Tasks from"./pages/Tasks";
-import Users from"./pages/Users";
+import Tasks from "./pages/Tasks";
+import Users from "./pages/Users";
 import Trash from "./pages/Trash";
 import TaskDetails from "./pages/TaskDetails";
 import Sidebar from "./components/Sidebar";
@@ -15,78 +15,78 @@ import { Fragment, useRef } from "react";
 import clsx from "clsx";
 import { IoClose } from "react-icons/io5";
 
-
-function Layout (){
+function Layout() {
   const { user } = useSelector(state => state.auth);
+  const location = useLocation();
 
-  const location = useLocation()
+  if (!user) {
+    return <Navigate to="/log-in" state={{ from: location }} replace />;
+  }
 
-  return user ?(
-    <div className="w-full h-screen flex flex-col md:flex-row">
-      <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
-        < Sidebar />
+  return (
+    <div className="flex flex-col w-full h-screen md:flex-row">
+      <div className="sticky top-0 hidden w-1/5 h-screen bg-white md:block">
+        <Sidebar />
       </div>
-
-      {/* <MobileSidebar/> */}
       <div className="flex-1 overflow-y-auto">
         <Navbar />
-
         <div className="p-4 2xl:p-10">
-          <Outlet/>
+          <Outlet />
         </div>
       </div>
     </div>
-
-  ):(
-    <Navigate to="/log-in" state={{from: location}} replace/>
-  )
+  );
 }
 
 const MobileSidebar = () => {
   const { isSidebarOpen } = useSelector((state) => state.auth);
-  const mobileMenuRef = useRef(null);
   const dispatch = useDispatch();
+  const mobileMenuRef = useRef(null);
 
   const closeSidebar = () => {
     dispatch(setOpenSidebar(false));
-  }
+  };
 
   return (
-  <>
     <Transition
       show={isSidebarOpen}
       as={Fragment}
       enter="transition-opacity duration-700"
-      enterFrom="opacity-x-10"
-      enterTo="opacity-x-100"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
       leave="transition-opacity duration-700"
-      leaveFrom="opacity-x-100"
-      leaveTo="opacity-x-0">
-        {(ref) => (
-          <div ref={(node) => (mobileMenuRef.current = node)} 
-          className={clsx("md:hidden w-full h-full bg-black/40 transition-all duration-700 transform ", 
-          isSidebarOpen ? "translate-x-0" : "translate-x-full")} onClick={()=> closeSidebar()}>
-            <div className="bg-white w-3/4 h-full">
-              <div className="w-full flex justify-end px-5 mt-5">
-                <button
-                  onClick={() => closeSidebar()}
-                  className="flex justify-end items-end"
-                >
-                  <IoClose size={25} />
-                </button>
-              </div>
-
-              <div className="-mt-10">
-                <Sidebar />
-              </div>
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+    >
+      {(ref) => (
+        <div
+          ref={mobileMenuRef}
+          className={clsx("md:hidden w-full h-full bg-black/40 transition-all duration-700 transform", 
+          isSidebarOpen ? "translate-x-0" : "translate-x-full")}
+          onClick={closeSidebar}
+          role="dialog"
+          aria-modal="true"
+        >
+          <div className="w-3/4 h-full bg-white">
+            <div className="flex justify-end w-full px-5 mt-5">
+              <button
+                onClick={closeSidebar}
+                className="flex items-end justify-end"
+                aria-label="Close sidebar"
+              >
+                <IoClose size={25} />
+              </button>
+            </div>
+            <div className="-mt-10">
+              <Sidebar />
             </div>
           </div>
-        )}
-        </Transition>
-    
-    </>
-  )
+        </div>
+      )}
+    </Transition>
+  );
 }
+
 function App() {
   return (
     <main className='w-full min-h-screen bg-[#f3f4f6]'>
@@ -102,13 +102,11 @@ function App() {
           <Route path="/trashed" element={<Trash />} />
           <Route path="/task/:id" element={<TaskDetails />} />
         </Route>
-
         <Route path="/log-in" element={<Login />} />
       </Routes>
-
-      <Toaster richColors/>
+      <Toaster richColors />
     </main>
   );
-};
+}
 
 export default App;
